561c69bbee56b22fc720c5a9d3b92a466efc277f
iAmMuadh <adeyemimuaz1@gmail.com>
2023-04-07 15:47:24 +0100

    added js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const faqItems = document.querySelectorAll('.FAQ li');
+
+faqItems.forEach(item => {
+    const spanToggle = item.querySelector('.span');
+    const displayMsg = item.querySelector('.answer');
+
+    const clickMe = () => {
+        displayMsg.classList.toggle('show')
+    };
+    spanToggle.addEventListener('click' , clickMe)
+    console.log(header)
+})
+
+
