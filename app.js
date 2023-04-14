6854e93668365a8f9db1afd605798e25d3d7e5cb
iAmMuadh <adeyemimuaz1@gmail.com>
2023-04-14 22:11:14 +0100

    ...

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const faqItems = document.querySelectorAll('.FAQ li');
+const faqQuestions = document.querySelectorAll('.question');
 
-faqItems.forEach(item => {
-    const spanToggle = item.querySelector('.span');
-    const displayMsg = item.querySelector('.answer');
+faqQuestions.forEach(item => {
+    const spanToggle = item.querySelector('.question-btn');
+    const displayMsg = item.querySelector('.question-text');
+    const questionSelector = item.querySelector('.question-selector')
 
-    const clickMe = () => {
-        displayMsg.classList.toggle('show')
+    const clickMe = (event) => {
+        displayMsg.classList.toggle('show-text')
+        questionSelector.classList.toggle('question-expanded-style')
+        // try commenting this out to see if clicking on the
+        // arrow works. Instead of clicking on the text.
+        event.stopPropagation()
     };
+
+    item.addEventListener('click', clickMe)
     spanToggle.addEventListener('click' , clickMe)
 })
-
-
